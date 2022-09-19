2014304b9fa9ff6513f85519149740ccbc75a03d
Enes Köksalmış <koksalmis@gmail.com>
2022-09-19 21:28:40 +0300

    about me and posts page added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 function HomePage() {
-  return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">
+            I'm Enes. I am a Sofware Engineer @Garanti BBVA Technology.
+          </h1>
+          <p>
+            I interested in Microservice Architecture, Docker, Kubernetes,
+            Desingn Patterns, Payment Systems
+          </p>
+
+          <p>In this blog, I will share my thoughts, projects and interests</p>
+        </div>
+      </div>
+      <div className=" max-w-4xl mx-auto mt-10">
+        <img src="/view.jpg" />
+      </div>
+    </>
+  )
 }
 
 export default HomePage
