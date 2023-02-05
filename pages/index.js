47a07e8722f220d96d0a85ad60ad7bdce9ad28fe
Enes Köksalmış <koksalmis@gmail.com>
2023-02-06 00:07:11 +0300

    reach me out added

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,20 @@ function HomePage() {
                                              className="font-medium text-blue-600 dark:text-blue-500 hover:underline">@Technology
                         of Money</a>
                     </h2>
-                    <p className="shine text-xl leading-snug">
-                        Interested in Microservices, Docker, Kubernetes,
+                    <p className="shine text-xl leading-snug">🌱 Interested in Microservices, Docker, Kubernetes,
                         Design Patterns, Payment Systems
                     </p>
                     <p className="shine text-xl leading-snug">In this blog, I will share my thoughts, projects and
                         interests</p>
+                    <div>
+                        <p>Reach me out via <a href="mailto:koksalmis@gmail.com"
+                                                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"> 📪
+                            Email</a>, <a
+                            href="https://www.linkedin.com/in/koksalmis/"
+                            className="font-medium text-blue-600 dark:text-blue-500 hover:underline">LinkedIn</a> or <a
+                            href="https://twitter.com/eneskoksalmis"
+                            className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Twitter 🐦</a></p>
+                    </div>
                 </div>
             </div>
             <div className="max-w-4xl mx-auto mt-20" align="center">
